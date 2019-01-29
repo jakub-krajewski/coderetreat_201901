c2b2eb9706603c590544ca329b19112f6f300194
Jakub Krajewski <jakub.krajewski@medius-group.pl>
2019-01-29 12:36:54 +0100

    ttt

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Tests
+﻿namespace Tests
 {
-    public class Class1
+    public class Tests
     {
     }
 }
